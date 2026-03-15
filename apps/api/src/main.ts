733b5bbf8e0768f92d87f2934ea7947b034313d4
Rajbha Manek <rajbha.manek.abjexperts@gmail.com>
2026-03-15 23:52:15 +0530

    fix: Use Socket.IO adapter for WebSocket
    Author: Rajbha Manek

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createServer } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const httpServer = createServer();
+  
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+  
   app.setGlobalPrefix('api');
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(process.env.PORT ?? 3000);
+  app.useWebSocketAdapter(new IoAdapter(httpServer));
+  
+  await app.listen(process.env.PORT ?? 3002);
+  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3002}`);
 }
 bootstrap();
